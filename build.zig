e0b65796ec51d1af72a14011d6fbc7bc230c6265
Vladimir Popov <vladimir@dokwork.ru>
2025-03-05 17:30:41 +0400

    refactor!: build script can build every example separatly

--- build.zig
+++ build.zig
@@ -27,31 +27,14 @@ pub fn build(b: *std.Build) void {
     });
     b.step("docs", "Build docs").dependOn(&install_docs.step);
 
-    // ----- Example -----
-    // to handle many examples see
-    // https://github.com/zigzap/zap/blob/master/build.zig#L49
-
-    const examples = b.addExecutable(.{
-        .name = "examples",
-        .root_source_file = b.path("examples/expression.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    examples.root_module.addImport("parcom", parcom);
-
-    b.installArtifact(examples);
-
-    const run_examples = b.addRunArtifact(examples);
-    if (b.args) |args| {
-        run_examples.addArgs(args);
-    }
-
-    const run_step = b.step("expression", "Run example of parsing a math expression");
-    run_step.dependOn(&run_examples.step);
-
     // ----- Unit tests -----
-    const test_filters = b.option([]const []const u8, "test-filter", "Skip tests that do not match any filter") orelse &[0][]const u8{};
+
+    // -Dtest-filter=<a part of the test name>
+    const test_filters = b.option(
+        []const []const u8,
+        "test-filter",
+        "Skip tests that do not match any filter",
+    ) orelse &[0][]const u8{};
 
     const parcom_tests = b.addTest(.{
         .root_source_file = b.path("src/parcom.zig"),
@@ -62,18 +45,40 @@ pub fn build(b: *std.Build) void {
 
     const run_parcom_tests = b.addRunArtifact(parcom_tests);
 
-    const example_tests = b.addTest(.{
-        .root_source_file = b.path("examples/expression.zig"),
-        .target = target,
-        .optimize = optimize,
-        .filters = test_filters,
-    });
-
-    example_tests.root_module.addImport("parcom", parcom);
-
-    const run_examples_tests = b.addRunArtifact(example_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_parcom_tests.step);
-    test_step.dependOn(&run_examples_tests.step);
+
+    // ----- Examples -----
+
+    inline for ([_][]const u8{ "expression" }) |example| {
+        const example_source = std.fmt.comptimePrint("examples/{s}.zig", .{example});
+        const exe = b.addExecutable(.{
+            .name = example,
+            .root_source_file = b.path(example_source),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        exe.root_module.addImport("parcom", parcom);
+
+        b.installArtifact(exe);
+
+        const run_exe = b.addRunArtifact(exe);
+        if (b.args) |args| {
+            run_exe.addArgs(args);
+        }
+        const run_step = b.step(example, std.fmt.comptimePrint("Run example of {s}", .{example}));
+        run_step.dependOn(&run_exe.step);
+
+        const example_tests = b.addTest(.{
+            .root_source_file = b.path(example_source),
+            .target = target,
+            .optimize = optimize,
+            .filters = test_filters,
+        });
+        example_tests.root_module.addImport("parcom", parcom);
+
+        const run_examples_tests = b.addRunArtifact(example_tests);
+        test_step.dependOn(&run_examples_tests.step);
+    }
 }
